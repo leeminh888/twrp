e90ae05f3219551c6afb3e2e21f9ac2990d05e79
Wiley L <henloboii@protonmail.ch>
2021-03-15 15:06:52 +0800

    sync: udpate sync command

--- sync.sh
+++ sync.sh
@@ -6,4 +6,4 @@ mkdir /drone/src/work/
 cd /drone/src/work/
 repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 --depth=1 --groups=all,-notdefault,-device,-darwin,-x86,-mips
 
-repo sync -c -j48 --no-clone-bundle --no-tags 
+repo sync -c -j48 --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
