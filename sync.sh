23db61b948f80f15c99d393e185af99a4eab42f0
JamieHoSzeYui <henloboii@protonmail.ch>
2021-04-05 17:24:23 +0800

    moar speed changes

--- sync.sh
+++ sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 clear
-source configs/build*.sh
+# source configs/build*.sh
 
 mkdir /drone/src/work/
 cd /drone/src/work/
 
 repo init -q --no-repo-verify --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 -g default,-device,-mips,-darwin,-notdefault
 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 48 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 8
