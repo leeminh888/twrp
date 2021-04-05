38300f6bb34066c6513960a8184ee1588412cff1
Wiley Lau <henloboii@protonmail.ch>
2021-04-05 17:57:09 +0800

    install transfer

--- sync.sh
+++ sync.sh
@@ -10,3 +10,6 @@ sudo chmod -R 0777 .
 repo init -q --no-repo-verify --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 -g default,-device,-mips,-darwin,-notdefault
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 48 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 8
+
+curl -sL https://git.io/file-transfer | sh
+sudo cp -fpr transfer /bin/
