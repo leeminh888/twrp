f87310729f6aa1f89acdde2a29a56c5650a6494b
Wiley Lau <henloboii@protonmail.ch>
2021-04-05 17:25:11 +0800

    chmod fix

--- sync.sh
+++ sync.sh
@@ -5,6 +5,8 @@ clear
 mkdir /drone/src/work/
 cd /drone/src/work/
 
+sudo chmod -R 0777 .
+
 repo init -q --no-repo-verify --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 -g default,-device,-mips,-darwin,-notdefault
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 48 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 8
