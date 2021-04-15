398ca61586eda368f91065b52db7b24a04f08187
leeminh888 <68751029+leeminh888@users.noreply.github.com>
2021-04-15 09:00:05 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,7 +7,7 @@ cd /drone/src/work/
 
 sudo chmod -R 0777 .
 
-repo init -q --no-repo-verify --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 -g default,-device,-mips,-darwin,-notdefault
+repo init -q --no-repo-verify --depth=1 -u git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-10.0 -g default,-device,-mips,-darwin,-notdefault
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 48 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 8
 
